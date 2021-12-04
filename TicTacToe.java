57292fd17cfb51cb4254532041940e14a3027a90
Tanya703 <90221377+Tanya703@users.noreply.github.com>
2021-12-04 13:50:24 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -20,7 +20,7 @@ public class TicTacToe {
 
 		
 
-	public static void printBoard(char[][]gameboard){
+	public static void printBoard(char[][]gameBoard){
                for (int i = 0; i < 3; i++) {
 
             for (int j = 0; j < 3; j++) {
@@ -188,6 +188,20 @@ public class TicTacToe {
  	   gameBoard[2][1] = '8';
  	   gameBoard[2][2] = '9';
  	}
+	
+public static boolean isWinningPattern(char[][] gameBoard) {
+  for(int i=0;i<3;i++){
+    if ((gameBoard[i][0]==gameBoard[i][1]) &&(gameBoard[i][1]==gameBoard[i][2]))
+      return true;
+    if((gameBoard[0][i]==gameBoard[1][i]) &&(gameBoard[1][i]==gameBoard[2][i]))
+      return true;
+  }
+  if ((gameBoard[0][0]==gameBoard[1][1]) &&(gameBoard[1][1]==gameBoard[2][2]))
+    return true;
+  if ((gameBoard[0][2]==gameBoard[1][1]) &&(gameBoard[1][1]==gameBoard[2][0]))
+    return true;
+  return false;
+}
 
 
 }
